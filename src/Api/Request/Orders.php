f0c2efec723f1f50a5f227dcf5a6f8a78b43ca93
Mateusz Nastalski <mnastalski@outlook.com>
2021-10-20 23:30:17 +0200

    Add more orders related requests

--- src/Api/Request/Orders.php
+++ src/Api/Request/Orders.php
@@ -10,10 +10,34 @@ class Orders extends Client implements OrdersInterface
     /**
      * @inheritDoc
      */
+    public function getJournalList(int $orderId, array $logsTypes, ?int $lastLogId = null): Response
+    {
+        return new Response(
+            $this->post('getJournalList', [
+                'last_log_id' => $lastLogId,
+                'logs_types' => $logsTypes,
+                'order_id' => $orderId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function addOrder(array $data): Response
     {
         return new Response(
             $this->post('addOrder', $data)
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrderSources(): Response
+    {
+        return new Response(
+            $this->post('getOrderSources')
         );
     }
 
@@ -30,6 +54,75 @@ class Orders extends Client implements OrdersInterface
     /**
      * @inheritDoc
      */
+    public function getOrderTransactionDetails(int $orderId): Response
+    {
+        return new Response(
+            $this->post('getOrderTransactionDetails', [
+                'order_id' => $orderId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrdersByEmail(string $email): Response
+    {
+        return new Response(
+            $this->post('getOrdersByEmail', [
+                'email' => $email,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrdersByPhone(string $phone): Response
+    {
+        return new Response(
+            $this->post('getOrdersByPhone', [
+                'phone' => $phone,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addInvoice(int $orderId, int $seriesId): Response
+    {
+        return new Response(
+            $this->post('addInvoice', [
+                'order_id' => $orderId,
+                'series_id' => $seriesId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getInvoices(array $data): Response
+    {
+        return new Response(
+            $this->post('getInvoices', $data)
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSeries(): Response
+    {
+        return new Response(
+            $this->post('getSeries')
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getOrderStatusList(): Response
     {
         return new Response(
@@ -40,12 +133,95 @@ class Orders extends Client implements OrdersInterface
     /**
      * @inheritDoc
      */
-    public function setOrderPayment(int $orderId, float $amount): Response
+    public function getOrderPaymentsHistory(int $orderId, bool $showFullHistory = false): Response
+    {
+        return new Response(
+            $this->post('getOrderPaymentsHistory', [
+                'order_id' => $orderId,
+                'show_full_history' => $showFullHistory,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getNewReceipts(int $seriesId): Response
+    {
+        return new Response(
+            $this->post('getNewReceipts', [
+                'series_id' => $seriesId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getReceipt(?int $receiptId = null, ?int $orderId = null): Response
+    {
+        return new Response(
+            $this->post('getReceipt', [
+                'receipt_id' => $receiptId,
+                'order_id' => $orderId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setOrderFields(array $data): Response
+    {
+        return new Response(
+            $this->post('setOrderFields', $data)
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addOrderProduct(array $data): Response
+    {
+        return new Response(
+            $this->post('addOrderProduct', $data)
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setOrderProductFields(array $data): Response
+    {
+        return new Response(
+            $this->post('setOrderProductFields', $data)
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteOrderProduct(int $orderId, int $orderProductId): Response
+    {
+        return new Response(
+            $this->post('deleteOrderProduct', [
+                'order_id' => $orderId,
+                'order_product_id' => $orderProductId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setOrderPayment(int $orderId, float $paymentDone, int $paymentDate, string $paymentComment): Response
     {
         return new Response(
             $this->post('setOrderPayment', [
                 'order_id' => $orderId,
-                'payment_done' => $amount,
+                'payment_done' => $paymentDone,
+                'payment_date' => $paymentDate,
+                'payment_comment' => $paymentComment,
             ])
         );
     }
@@ -59,6 +235,35 @@ class Orders extends Client implements OrdersInterface
             $this->post('setOrderStatus', [
                 'order_id' => $orderId,
                 'status_id' => $statusId,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setOrderReceipt(int $receiptId, string $receiptNr, int $date, bool $printerError = false): Response
+    {
+        return new Response(
+            $this->post('setOrderReceipt', [
+                'order_id' => $receiptId,
+                'receipt_nr' => $receiptNr,
+                'date' => $date,
+                'printer_error' => $printerError,
+            ])
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addOrderInvoiceFile(int $invoiceId, string $file, string $externalInvoiceNumber): Response
+    {
+        return new Response(
+            $this->post('addOrderInvoiceFile', [
+                'invoice_id' => $invoiceId,
+                'file' => $file,
+                'external_invoice_number' => $externalInvoiceNumber,
             ])
         );
     }
--- src/Api/Request/OrdersInterface.php
+++ src/Api/Request/OrdersInterface.php
@@ -7,10 +7,23 @@ use Baselinker\Api\Response\Response;
 interface OrdersInterface
 {
     /**
+     * @param int $orderId
+     * @param array $logsTypes
+     * @param int|null $lastLogId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getJournalList(int $orderId, array $logsTypes, ?int $lastLogId = null): Response;
+
+    /**
      * @param array $data
      * @return \Baselinker\Api\Response\Response
      */
     public function addOrder(array $data): Response;
+
+    /**
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getOrderSources(): Response;
 
     /**
      * @param array $data
@@ -19,16 +32,99 @@ interface OrdersInterface
     public function getOrders(array $data): Response;
 
     /**
+     * @param int $orderId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getOrderTransactionDetails(int $orderId): Response;
+
+    /**
+     * @param string $email
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getOrdersByEmail(string $email): Response;
+
+    /**
+     * @param string $phone
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getOrdersByPhone(string $phone): Response;
+
+    /**
+     * @param int $orderId
+     * @param int $seriesId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function addInvoice(int $orderId, int $seriesId): Response;
+
+    /**
+     * @param array $data
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getInvoices(array $data): Response;
+
+    /**
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getSeries(): Response;
+
+    /**
      * @return \Baselinker\Api\Response\Response
      */
     public function getOrderStatusList(): Response;
 
     /**
      * @param int $orderId
-     * @param float $amount
+     * @param bool $showFullHistory
      * @return \Baselinker\Api\Response\Response
      */
-    public function setOrderPayment(int $orderId, float $amount): Response;
+    public function getOrderPaymentsHistory(int $orderId, bool $showFullHistory = false): Response;
+
+    /**
+     * @param int $seriesId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getNewReceipts(int $seriesId): Response;
+
+    /**
+     * @param int|null $receiptId
+     * @param int|null $orderId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function getReceipt(?int $receiptId = null, ?int $orderId = null): Response;
+
+    /**
+     * @param array $data
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function setOrderFields(array $data): Response;
+
+    /**
+     * @param array $data
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function addOrderProduct(array $data): Response;
+
+    /**
+     * @param array $data
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function setOrderProductFields(array $data): Response;
+
+    /**
+     * @param int $orderId
+     * @param int $orderProductId
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function deleteOrderProduct(int $orderId, int $orderProductId): Response;
+
+    /**
+     * @param int $orderId
+     * @param float $paymentDone
+     * @param int $paymentDate
+     * @param string $paymentComment
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function setOrderPayment(int $orderId, float $paymentDone, int $paymentDate, string $paymentComment): Response;
 
     /**
      * @param int $orderId
@@ -36,4 +132,21 @@ interface OrdersInterface
      * @return \Baselinker\Api\Response\Response
      */
     public function setOrderStatus(int $orderId, int $statusId): Response;
+
+    /**
+     * @param int $receiptId
+     * @param string $receiptNr
+     * @param int $date
+     * @param bool $printerError
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function setOrderReceipt(int $receiptId, string $receiptNr, int $date, bool $printerError = false): Response;
+
+    /**
+     * @param int $invoiceId
+     * @param string $file
+     * @param string $externalInvoiceNumber
+     * @return \Baselinker\Api\Response\Response
+     */
+    public function addOrderInvoiceFile(int $invoiceId, string $file, string $externalInvoiceNumber): Response;
 }
