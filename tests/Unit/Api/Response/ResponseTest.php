b9c3cb9da082df82d0581946919380748dc17be5
Mateusz Nastalski <mnastalski@outlook.com>
2024-06-10 13:28:00 +0200

    Add tests

---
+++ tests/Unit/Api/Response/ResponseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Api\Response;
+
+use Baselinker\Api\Response\BaselinkerException;
+use Baselinker\Api\Response\Response;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use PHPUnit\Framework\TestCase;
+
+class ResponseTest extends TestCase
+{
+    public function testGetters(): void
+    {
+        $body = json_encode([
+            'status' => 'SUCCESS',
+            'list' => [
+                'foo' => 'bar',
+            ],
+        ]);
+
+        $response = new Response(
+            new GuzzleResponse(200, [], $body)
+        );
+
+        $this->assertEquals('{"status":"SUCCESS","list":{"foo":"bar"}}', $response->contents());
+
+        $this->assertEquals([
+            'status' => 'SUCCESS',
+            'list' => [
+                'foo' => 'bar',
+            ],
+        ], $response->toArray());
+
+        $this->assertEquals([
+            'foo' => 'bar',
+        ], $response->getParameter('list'));
+    }
+
+    public function testWithErrorThrowsException(): void
+    {
+        $body = json_encode([
+            'status' => 'ERROR',
+            'error_code' => 'CODE',
+            'error_message' => 'Message',
+        ]);
+
+        $this->expectException(BaselinkerException::class);
+        $this->expectExceptionMessage('[CODE] Message');
+
+        new Response(
+            new GuzzleResponse(200, [], $body)
+        );
+    }
+}
