10c69b49cb34d2327a21a41790d5a78338be3b2b
Mateusz Nastalski <mnastalski@outlook.com>
2024-10-24 13:39:40 +0200

    Update parameters in `BaselinkerConnect` endpoints

--- src/Api/Request/BaselinkerConnect.php
+++ src/Api/Request/BaselinkerConnect.php
@@ -14,29 +14,29 @@ class BaselinkerConnect extends Client
         );
     }
 
-    public function getConnectIntegrationContractors(int $tokenId): Response
+    public function getConnectIntegrationContractors(int $connectIntegrationId): Response
     {
         return new Response(
             $this->post('getConnectIntegrationContractors', [
-                'token_id' => $tokenId,
+                'connect_integration_id' => $connectIntegrationId,
             ])
         );
     }
 
-    public function getConnectContractorCreditHistory(int $contractorId): Response
+    public function getConnectContractorCreditHistory(int $connectContractorId): Response
     {
         return new Response(
             $this->post('getConnectContractorCreditHistory', [
-                'contractor_id' => $contractorId,
+                'connect_contractor_id' => $connectContractorId,
             ])
         );
     }
 
-    public function addConnectContractorCredit(int $contractorId, float $amount, string $message): Response
+    public function addConnectContractorCredit(int $connectContractorId, float $amount, string $message): Response
     {
         return new Response(
             $this->post('addConnectContractorCredit', [
-                'contractor_id' => $contractorId,
+                'connect_contractor_id' => $connectContractorId,
                 'amount' => $amount,
                 'message' => $message,
             ])
